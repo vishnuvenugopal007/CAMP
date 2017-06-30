f906880450a016cd7915e212ec32a82765276b56
Vishnu <vishnu_venugopal@live.com>
2017-06-30 00:35:30 +0000

    Added Jumbotron, implemented Responsive Grid

--- v1/app.js
+++ v1/app.js
@@ -13,13 +13,16 @@ var campgrounds = [
         {name: 'Salmon Creek' , image:'https://farm3.staticflickr.com/2286/2270488741_2a1b9822a6.jpg'},
         {name: 'Granite Hill', image:'https://farm6.staticflickr.com/5570/14879150454_848a44fb06.jpg'},
         {name: 'Mount Homer', image:'https://farm3.staticflickr.com/2839/11407596925_a0f9f4abf0.jpg'},
-        {name: 'Silent Silver Forest', image:'https://farm4.staticflickr.com/3069/2942421645_38b206298a.jpg'},
+        {name: 'Silver Lake', image:'https://farm8.staticflickr.com/7250/7467209450_a8df729b57.jpg'},
+        {name: 'Crater Lake', image:'https://farm9.staticflickr.com/8020/7538732802_49a42d28d2.jpg'},
+        {name: 'Salmon Creek' , image:'https://farm3.staticflickr.com/2286/2270488741_2a1b9822a6.jpg'},
+        {name: 'Granite Hill', image:'https://farm6.staticflickr.com/5570/14879150454_848a44fb06.jpg'},
+        {name: 'Mount Homer', image:'https://farm3.staticflickr.com/2839/11407596925_a0f9f4abf0.jpg'},
+        {name: 'Silver Lake', image:'https://farm8.staticflickr.com/7250/7467209450_a8df729b57.jpg'},
         {name: 'Crater Lake', image:'https://farm9.staticflickr.com/8020/7538732802_49a42d28d2.jpg'}
 ]
 
 app.get('/campgrounds', function(req, res){
-
-    
      res.render('campgrounds', {campgrounds:campgrounds})
 })
 
