12cd88ca4f9f60d7e2b6274948cd94c14b5870c0
Vishnu <vishnu_venugopal@live.com>
2017-08-11 18:59:39 +0000

    Fixed Mongo Deprecation Error

--- v13/app.js
+++ v13/app.js
@@ -15,11 +15,12 @@ var seedDB = require('./seeds');
 var commentRoutes = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index')
-    
+
+console.log(process.env.DATABASEURL);    
 // var url = process.env.DATABASEURL || "mongodb://localhost/CAMP_v13";
 // mongoose.connect('mongodb://localhost/CAMP_v13');
 var url = process.env.DATABASEURL || "mongodb://vishnu:childish7@ds161001.mlab.com:61001/vcamp"
-mongoose.connect('mongodb://vishnu:childish7@ds161001.mlab.com:61001/vcamp');
+mongoose.connect('mongodb://vishnu:childish7@ds161001.mlab.com:61001/vcamp', {useMongoClient: true});
 //mongodb://vishnu:childish7@ds161001.mlab.com:61001/vcamp
 
 app.use(bodyParser.urlencoded({extended: true}));
